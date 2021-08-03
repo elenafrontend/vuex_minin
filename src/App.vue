822f4a58fb7155001552fd481b5d7b0d2be1d291
gumerova.e <gumerova.e@etton.ru>
2021-08-03 15:47:18 +0300

    get posts from server with fetch

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <article class="post" v-for='post of posts' :key='post.id'>
+      <h2 class="post__title">{{ post.title }}</h2>
+      <p class="post__body">{{ post.body }}</p>
+    </article>
   </div>
 </template>
 
@@ -7,6 +11,18 @@
 
 export default {
   name: 'App',
+
+  data() {
+    return {
+      posts: []
+    } 
+  },
+
+  async mounted() {
+    const responce = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3')
+    const posts = await responce.json()
+    this.posts = posts
+  }
 }
 </script>
 
@@ -17,6 +33,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 400px;
+}
+
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+
 }
 </style>
